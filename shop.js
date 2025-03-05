f9cb8c0daa3b42c62c9bf1ecb2b81dfdbbab0cdc
NoodGreen <rushikeshbommasani@gmail.com>
2025-03-05 13:40:00 +0530

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,32 +1,13 @@
-//project details
-//express (for making websites shop)
-
-//modules
-const express = require("express"); //handle requests and responses, eg: listen to shop. -2
-const fs = require("fs"); //file system will handle, system files. -4
+const express = require("express");
+const fs = require("fs");
 const morgan = require("morgan");
-//own module importing router and use
+
 const imagesRouter = require("./imagesRouter");
 
-//store
-const shop = express(); //create app or web. eg: shop -1
-
-shop.use(express.json()); //shop.use() runs code before requests reach route handlers,  -6
-//express.json() Required for handling JSON request bodies accessing from frontend.
-
+const shop = express();
+shop.use(express.json());
 shop.use("/images", imagesRouter);
 
-//server -3
 shop.listen(3000, "localhost", () => {
-  //3000 shop address.
-  // localhost shop is only open on your own computer, not the internet,
-  // () callback funtion is like an alarm that rings when the shop opens.
   console.log("🔔 Shop is open at http://localhost:3000");
 });
-
-//more
-//////////////
-// JSON is a way to store and send data.
-// {} Objects eg: box that holds key-value pairs.
-// [] Arrays eg: list of values.
-//////////////
