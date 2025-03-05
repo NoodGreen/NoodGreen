695a8a2be22d56d33a6ca89ea675bc74ffff145f
NoodGreen <rushikeshbommasani@gmail.com>
2025-03-05 13:40:31 +0530

    Update imagesRouter.js

--- imagesRouter.js
+++ imagesRouter.js
@@ -1,10 +1,9 @@
-//methode.3 //export module router to shop.js
 const express = require("express");
-const router = express.Router(); //Import router
+const router = express.Router();
 const imageController = require("./imagesController");
 
 router.param("id", imageController.checkID);
 
-router.route("/").get(imageController.getAllImages); //Attach router to `/` path
+router.route("/").get(imageController.getAllImages);
 router.route("/:id").get(imageController.getImage);
 module.exports = router;
