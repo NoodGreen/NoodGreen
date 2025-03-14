8d2a5ad4d37f802dc999eb525b46877ffdc604ef
NoodGreen <rushikeshbommasani@gmail.com>
2025-03-14 23:58:52 +0530

    Delete imagesRouter.js

--- imagesRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const imageController = require("./imagesController");
-
-router.param("id", imageController.checkID);
-
-router.route("/").get(imageController.getAllImages);
-router.route("/:id").get(imageController.getImage);
-module.exports = router;
